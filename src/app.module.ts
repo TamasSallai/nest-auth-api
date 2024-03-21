d97b51967df1d61eb2855187d1e112850fff9a33
Sallai Tamás <tamas.sallai.hu@gmail.com>
2024-03-21 14:58:43 +0100

    added nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, PrismaModule],
 })
 export class AppModule {}
